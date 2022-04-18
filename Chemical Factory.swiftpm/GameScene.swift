736097fcdfa9b08ec46dee3eb1b71dbd5636d50e
DaneOnishi <daniellaonishi07@gmail.com>
2022-04-18 00:27:14 -0300

    Initial commit

---
+++ Chemical Factory.swiftpm/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Chemical Factory
+//
+//  Created by Daniella Onishi on 18/04/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+    
+}
+
