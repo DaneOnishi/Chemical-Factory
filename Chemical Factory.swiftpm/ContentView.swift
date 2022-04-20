a21cfb90eacee417179042f5f09120459a0c6724
DaneOnishi <daniellaonishi07@gmail.com>
2022-04-20 18:15:03 -0300

    Adds assets

--- Chemical Factory.swiftpm/ContentView.swift
+++ Chemical Factory.swiftpm/ContentView.swift
@@ -1,12 +1,9 @@
 import SwiftUI
+import SpriteKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        SpriteView(scene: GameScene(fileNamed: "GameScene")!)
+            
     }
 }
